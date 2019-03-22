5e8cae087ff0b652c82711aafebd5f69aa20bef2
Tosouki <qwert60210@yahoo.com.tw>
2019-03-22 14:28:15 +0800

    Create Q2_Byte.java

---
+++ 第二周-HW02/Q2_Byte.java
@@ -0,0 +1,8 @@
+public class Q2_Byte{
+	public static void main(String[] args){
+		Byte b = (byte)(015 + 0xff2a);
+		Byte tatic = 3;
+		System.out.println("b = " + b);
+		System.out.println("static = " + tatic);
+	}
+}
