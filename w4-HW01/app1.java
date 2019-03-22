8bed5277cc2d96e581a2d9abbbdb5d840db36bd2
Tosouki <qwert60210@yahoo.com.tw>
2019-03-22 15:18:49 +0800

    Create app1.java

---
+++ w4-HW01/app1.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class app1{
+        public static void main(String[]args)throws IOException{
+               BufferedReader buf=new BufferedReader(new InputStreamReader(System.in));
+
+               int min,max,base,a=1,b;
+               
+               System.out.print("請輸入範圍最小數:");
+               min =Integer.parseInt(buf.readLine());
+               
+               System.out.print("請輸入範圍最大數:");
+               max =Integer.parseInt(buf.readLine());
+               
+               System.out.print("請輸入要尋找的倍數的基值:");
+               base =Integer.parseInt(buf.readLine());
+               
+               if(max>min){
+               System.out.println(base+"的倍數為");
+               b=base;
+                       while(min<b&&b<max){
+                        b=base*a;
+                        System.out.print(b+" ");
+                        a++;    
+                      }
+               }
+              else
+              System.out.println("錯誤!");
+        }
+}
