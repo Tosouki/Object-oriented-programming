b60e3fa2d382b9f532607630586c1b4ffc76f64e
Tosouki <qwert60210@yahoo.com.tw>
2019-03-22 14:40:27 +0800

    Create app1.java

---
+++ 第三周-HW01/app1.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class app1{
+        public static void main(String[]args)throws IOException{
+                BufferedReader buf;
+                String str,sex,bir,from;
+                buf=new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("請輸入你的姓名:");
+                str = buf.readLine();
+                buf=new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("請輸入你的性別:");
+                sex = buf.readLine();
+                buf=new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("請輸入你的西元出生年份:");
+                bir = buf.readLine();
+                buf=new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("請輸入你的出生地:");
+                from = buf.readLine();
+                System.out.println("姓名:"+str);
+                System.out.println("性別:"+sex);
+                System.out.println("西元出生年份:"+bir);
+                System.out.println("出生地:"+from);
+        }
+}
