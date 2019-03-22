399b51a52919af14cd1055ebb55740417bea1cae
Tosouki <qwert60210@yahoo.com.tw>
2019-03-23 06:40:37 +0800

    Create app1.java

---
+++ w13-HW02/app1.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class app1 {
+	public static void main(String arg[]) throws IOException {
+		BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("請輸入帳號：");
+		String a = buf.readLine();
+		System.out.print("請輸入密碼：");
+		String p = buf.readLine();
+		String p2;
+		do{
+		System.out.print("請再次輸入密碼：");
+		p2 = buf.readLine();
+		}while(!p2.equals(p));
+		Game_account c = new Game_account(a,p);
+		c.showProfile();
+	}
+}
