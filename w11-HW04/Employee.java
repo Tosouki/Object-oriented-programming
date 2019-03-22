f9cf6503465fe608cfaf0ad0649eed2cee21e174
Tosouki <qwert60210@yahoo.com.tw>
2019-03-23 06:32:35 +0800

    Create Employee.java

---
+++ w11-HW04/Employee.java
@@ -0,0 +1,54 @@
+class Employee {
+          private String name; 
+          private String telephone;     
+          private int year;  
+
+          Employee(String name,String telephone,int year) {
+            setName(name);
+            settelephone(telephone);
+            setyear(year);
+          }
+
+          void setName(String name) {
+            this.name = name;
+          }
+
+          String getName() {
+            return name;
+          }
+
+          void settelephone(String telephone) {
+            int i,a;
+            char telephone2[];
+            telephone2=telephone.toCharArray();
+            for(i=0,a=0;i<telephone2.length;i++){
+              if(Character.isDigit(telephone2[i])){
+              a++;
+                        }
+                      }
+            if(telephone2.length==10&&telephone2[0]=='0'&&telephone2[1]=='9'&&a==10){
+            this.telephone = telephone;}
+            else{
+            this.telephone ="0912345678";}	
+          }
+
+          String gettelephone() {
+            return telephone;
+          }
+
+          void setyear(int year) {
+            if(year>=1&&year<=30)
+            this.year = year;
+            else{
+            this.year = 1;}
+          }
+
+          int getyear() {
+            return year;
+          }
+
+
+          void showProfile() {
+            System.out.println("員工姓名：" + name + "\n員工手機：" + telephone + "\n員工年資：" + year );
+          }
+}
