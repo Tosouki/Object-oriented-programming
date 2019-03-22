02ef3379c79e83888a83fb46e05c4f2ee7a17529
Tosouki <qwert60210@yahoo.com.tw>
2019-03-23 06:55:37 +0800

    Create app1.java

---
+++ w14-HW01/app1.java
@@ -0,0 +1,10 @@
+public class app1{
+	public static void main(String args[]){
+		Employee test1 = new Basic_Employee("John","男","0933-1234","American",2);
+		test1.show();
+		Employee test2 = new managers("Nancy","女","0933-4321","Japan",2);
+		test2.show();
+		Employee test3 = new supervisors("John","男","7533967","Taiwna",2);
+		test3.show();
+						}
+}
