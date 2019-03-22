6df261dffe373b1c452a3934fbd454fb4409583b
Tosouki <qwert60210@yahoo.com.tw>
2019-03-22 14:35:18 +0800

    Create P2.java

---
+++ 第二周-HW04/P2.java
@@ -0,0 +1,11 @@
+public class P2{
+            public static void main(String[]args){
+
+                char word_1 =74  ;
+                char word_2 =97  ;
+                char word_3 =118 ;
+                char word_4 =97  ;
+                System.out.println("結果:"+word_1+word_2+word_3+word_4);
+
+        }
+}
