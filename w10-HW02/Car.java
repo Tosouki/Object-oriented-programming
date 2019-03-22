bf8dcf5cf3fa9aa231d4e46f685f76edf263a5f8
Tosouki <qwert60210@yahoo.com.tw>
2019-03-23 06:14:35 +0800

    Create Car.java

---
+++ w10-HW02/Car.java
@@ -0,0 +1,43 @@
+class Car{
+          private String modelName;
+          private int carLength,engCC,maxSpeed;
+          public Car(){
+            this("",0,0,0);
+          }
+          public Car(String modelName){
+            this(modelName,0,0,0);
+          }
+          public Car(String modelName,int carLength){
+            this(modelName,carLength,0,0);
+          }		
+          public Car(String modelName,int carLength,int engCC,int maxSpeed){
+            setModel(modelName);
+            setCarlength(carLength);
+            setEngCC(engCC);
+            setMaxSpeed(maxSpeed);
+          }
+          public void setModel(String name){
+            modelName=name;
+          }
+          public void setCarlength(int length){
+            if(length<0)
+            carLength=0;
+            else		
+            carLength=length;
+          }
+          public void setEngCC(int engcc){
+            if(engcc>5000||engcc<2000)
+            engCC=0;
+            else				
+            engCC=engcc;
+          }
+          public void setMaxSpeed(int speed){
+            if(speed>400||speed<0)
+            maxSpeed=0;
+            else
+            maxSpeed=speed;
+          }
+          void showProfile(){
+          System.out.println("品牌名稱:"+modelName+"\n車身長度:"+carLength+"\n汽缸CC數:"+engCC+"\n最高車速:"+maxSpeed);
+        }
+}
